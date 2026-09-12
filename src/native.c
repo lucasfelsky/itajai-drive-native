72cacd555c2e2742afbc1c84c26aa4f22f2d02de
lucas <63211460+lucasfelsky@users.noreply.github.com>
2026-09-11 23:44:39 -0300

    Add native engine compilation unit

---
+++ src/native.c
@@ -0,0 +1,10 @@
+// Itajai Drive Native source bundle.
+// Split into include parts only to keep connector-sized commits manageable.
+#include "native_parts/part01.inc"
+#include "native_parts/part02.inc"
+#include "native_parts/part03.inc"
+#include "native_parts/part04.inc"
+#include "native_parts/part05.inc"
+#include "native_parts/part06.inc"
+#include "native_parts/part07.inc"
+#include "native_parts/part08.inc"
