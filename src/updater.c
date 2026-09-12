4c15be51017da303cb12896491c1daeeeafe2fa4
lucas <63211460+lucasfelsky@users.noreply.github.com>
2026-09-11 23:46:14 -0300

    Add updater compilation unit

---
+++ src/updater.c
@@ -0,0 +1,7 @@
+// Itajai Drive Updater source bundle.
+// Split into include parts only to keep connector-sized commits manageable.
+#include "updater_parts/part01.inc"
+#include "updater_parts/part02.inc"
+#include "updater_parts/part03.inc"
+#include "updater_parts/part04.inc"
+#include "updater_parts/part05.inc"
