7266630891849f7ef40d5de14e15131da103b05a
lucas <63211460+lucasfelsky@users.noreply.github.com>
2026-09-12 01:20:28 -0300

    Wire 0.13 PBR compatibility renderer

--- src/native.c
+++ src/native.c
@@ -126,7 +126,16 @@
 #undef draw_roads
 #undef draw_buildings
 #undef draw_hud
+#define draw_ground draw_ground_legacy12
+#define draw_roads draw_roads_legacy12
+#define draw_buildings draw_buildings_legacy12
+#define draw_hud draw_hud_legacy12
 #include "render/render12.inc"
+#undef draw_ground
+#undef draw_roads
+#undef draw_buildings
+#undef draw_hud
+#include "render/render13.inc"
 
 #define draw_loading draw_loading_legacy06
 #define render render_legacy06
